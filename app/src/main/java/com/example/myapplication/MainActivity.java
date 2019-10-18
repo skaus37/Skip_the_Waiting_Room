3196031b9be5e7661851e851ee4d4e29194ddfca
Elian Karpov <elian1741@gmail.com>
2019-10-17 21:33:00 -0400

    :)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String firstName, lastName, username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void createAccountClick(View view) {
+        Intent intent = new Intent (this, create_account.class);
+        startActivity(intent);
+    }
+
+    public void logInClick(View view){
+
+    }
+
 }
---
+++ app/src/main/java/com/example/myapplication/create_account.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_account extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
