f596939bea2c00f0dcc5df5625d60df4c5924ce0
Elian Karpov <elian1741@gmail.com>
2019-10-17 22:53:33 -0400

    create account page

--- app/src/main/java/com/example/myapplication/create_account.java
+++ app/src/main/java/com/example/myapplication/create_account.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
 
 public class create_account extends AppCompatActivity {
@@ -11,5 +13,11 @@ public class create_account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+
+
+    public void goBack(View view){
+        Intent intent = new Intent (this, MainActivity.class);
+        startActivity(intent);
     }
 }
