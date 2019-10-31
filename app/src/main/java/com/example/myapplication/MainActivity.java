9257fc856fd17c92bab958595eb4232318ce6df8
Angus Young <angusryoung@gmail.com>
2019-10-31 03:24:59 -0400

    you can now log in using an account that you made using the email and password

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.os.Bundle;
@@ -7,15 +8,25 @@ import android.content.Intent;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private String userName, passWord;
+    private String email, passWord;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -28,13 +39,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void logInClick(View view){
-        EditText userText = (EditText)findViewById(R.id.username);
+        EditText emailText = (EditText)findViewById(R.id.email_Login);
         EditText passText = (EditText)findViewById(R.id.password);
-        userName = userText.getText().toString();
+        email = emailText.getText().toString();
         passWord = passText.getText().toString();
-        if (userName.matches("") || passWord.matches("")) {
+        if (email.matches("") || passWord.matches("")) {
             Toast.makeText(this, "You need to enter a username and a password", Toast.LENGTH_SHORT).show();
             return; }
+        else{
+            mAuth.signInWithEmailAndPassword(email, passWord)
+                    .addOnCompleteListener((@NonNull Task<AuthResult> task)->{
+
+
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                Intent intent = new Intent (this, LogIn.class);
+                                startActivity(intent);
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+
+                                Toast.makeText(this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                return ;
+                                
+                            }
+
+                            // ...
+
+                    });
+
+
+        }/*
+        else{
+            Toast.makeText(this, "You need to enter a valid username and a password", Toast.LENGTH_SHORT).show();
+        }*/
 
     }
 
