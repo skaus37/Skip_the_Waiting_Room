408325ed63128fd97b43149b394e1314449b10df
Simrun Kaushal <skaus027@uottawa.ca>
2019-11-05 15:40:19 -0500

    adding email to login screen

--- app/src/main/java/com/example/myapplication/LogIn.java
+++ app/src/main/java/com/example/myapplication/LogIn.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -11,6 +12,11 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class LogIn extends AppCompatActivity {
@@ -31,7 +37,21 @@ public class LogIn extends AppCompatActivity {
 
         FirebaseUser user = mAuth.getCurrentUser();
         String email = user.getEmail();
+        /*
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("users"+"/"+email);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //String name = dataSnapshot.getValue(firstName.class);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        }
+        */
 
         textView.setText("text you want to display "+email);
 
