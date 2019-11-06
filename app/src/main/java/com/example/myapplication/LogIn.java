293af4c790b68c609efa5ff658440a5f5cecb637
Angus Young <angusryoung@gmail.com>
2019-11-05 21:12:35 -0500

    added account type thingy

--- app/src/main/java/com/example/myapplication/LogIn.java
+++ app/src/main/java/com/example/myapplication/LogIn.java
@@ -48,7 +48,8 @@ public class LogIn extends AppCompatActivity {
         database.collection("users").document(email).get().addOnCompleteListener((@NonNull Task<DocumentSnapshot> task)->{
             if (task.isSuccessful()) {
                 String firstName = task.getResult().get("firstName").toString();
-                textView.setText("text you want to display "+firstName);
+                String account = task.getResult().get("accountType").toString();
+                textView.setText("Hello "+firstName+", you are a "+account);
             }
         });
 
