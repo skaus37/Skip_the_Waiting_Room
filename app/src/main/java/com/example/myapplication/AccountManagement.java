ddf047be4f1a60576324365e492788b2e47e43d5
Angus Young <angusryoung@gmail.com>
2019-11-09 15:33:38 -0500

    email now load on the activity account

--- app/src/main/java/com/example/myapplication/AccountManagement.java
+++ app/src/main/java/com/example/myapplication/AccountManagement.java
@@ -1,34 +1,92 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Document;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AccountManagement extends AppCompatActivity {
-    DatabaseReference databaseAccounts;
-    List<User> users;
-    ListView listViewUsers;
-    Button DeleteButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_management);
-        listViewUsers = (ListView) findViewById(R.id.listOfUsers);
-        DeleteButton = (Button)findViewById(R.id.delete_account);
-        users = new ArrayList<User>();
-        databaseAccounts = FirebaseDatabase.getInstance().getReference("users");
+
+        updateListVeiw();
+        /*
+
+
+        ListView attempt = (ListView) findViewById(R.id.listOfUsers);
+
+        String[] fruits = new String[] {
+                "Cape Gooseberry",
+                "Capuli cherry"
+        };
+        /*
+        List<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, fruits_list);
+        attempt.setAdapter(arrayAdapter);*/
+
+
     }
+
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+
+    public void updateListVeiw(){
+        //ArrayList<String> accountArray;
+
+
+        database.collection("users").get().addOnCompleteListener((@NonNull Task<QuerySnapshot> task)->{
+            if (task.isSuccessful()){
+
+                String currentUser;
+                ArrayList<String> accountArray = new ArrayList<>();
+                //make it impossible for the admin to deleate admin account
+                for (QueryDocumentSnapshot document : task.getResult()){
+                    currentUser = document.getId();
+                    accountArray.add(currentUser);
+
+                }
+                ListView userListView = (ListView) findViewById(R.id.listOfUsers);
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                        (this, android.R.layout.simple_list_item_1, accountArray);
+                userListView.setAdapter(arrayAdapter);
+
+            }
+
+        });
+
+
+
+
+    }
+
+
+
+    /*
     @Override
     protected  void onStart() {
         super.onStart();
@@ -50,5 +108,5 @@ public class AccountManagement extends AppCompatActivity {
             }
         });
 
-    }
+    }*/
 }
