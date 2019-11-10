d022b823507add8e53a406139d75006c5538d42d
Simrun Kaushal <skaus027@uottawa.ca>
2019-11-09 22:23:38 -0500

    delete feature and update list

--- app/src/main/java/com/example/myapplication/AccountManagement.java
+++ app/src/main/java/com/example/myapplication/AccountManagement.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,11 +15,6 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -31,7 +27,6 @@ import java.util.List;
 
 public class AccountManagement extends AppCompatActivity {
     ListView userListView;
-    DatabaseReference mDatabase;
     ArrayList<String> accountArray;
 
     @Override
@@ -43,18 +38,6 @@ public class AccountManagement extends AppCompatActivity {
 
         updateListVeiw();
         deleteAccounts();
-        /*
-        ListView attempt = (ListView) findViewById(R.id.listOfUsers);
-
-        String[] fruits = new String[] {
-                "Cape Gooseberry",
-                "Capuli cherry"
-        };
-        /*
-        List<String> fruits_list = new ArrayList<String>(Arrays.asList(fruits));
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1, fruits_list);
-        attempt.setAdapter(arrayAdapter);*/
 
 
     }
@@ -87,17 +70,22 @@ public class AccountManagement extends AppCompatActivity {
 
 
     public void deleteAccounts() {
-        TextView textView = (TextView) findViewById(R.id.textView4);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        database = FirebaseFirestore.getInstance();
         userListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String email = accountArray.get(i);
-                textView.setText(email);
+                database.collection("users").document(email).delete();
+                updateListVeiw();
                 return true;
             }
 
         });
+    }
+    public void goBack(View view){
+        Intent intent = new Intent(this, admin_activity.class);
+        startActivity(intent);
+
     }
 }
