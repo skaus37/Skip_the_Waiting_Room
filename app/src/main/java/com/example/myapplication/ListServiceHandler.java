56e31924afa33f4cad145c822811715c37049d5b
Simrun Kaushal <skaus027@uottawa.ca>
2019-11-19 23:43:02 -0500

    Adding services to user firebase

--- app/src/main/java/com/example/myapplication/ListServiceHandler.java
+++ app/src/main/java/com/example/myapplication/ListServiceHandler.java
@@ -1,19 +1,32 @@
 package com.example.myapplication;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ListServiceHandler extends BaseAdapter implements ListAdapter {
     private ArrayList<String> list = new ArrayList<String>();
@@ -41,6 +54,7 @@ public class ListServiceHandler extends BaseAdapter implements ListAdapter {
 
     @Override
     public View getView (final int position, View convertView, ViewGroup parent){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
         View view = convertView;
         if (view == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -48,6 +62,40 @@ public class ListServiceHandler extends BaseAdapter implements ListAdapter {
         }
         TextView listItemText = (TextView)view.findViewById(R.id.serviceName);
         listItemText.setText(list.get(position));
+        Button addButton = (Button)view.findViewById(R.id.Add_Button);
+        Button deleteButton = (Button)view.findViewById((R.id.deleteButton));
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = list.get(position);
+                //database.collection("services").get().addOnCompleteListener((@NonNull Task<QuerySnapshot> task) -> {
+                database.collection("services").document(id).get().addOnCompleteListener((@NonNull Task<DocumentSnapshot> task)->{
+                    if (task.isSuccessful()) {
+                        String roleName = task.getResult().get("role").toString();
+                        String nameService = task.getResult().get("Name").toString();
+                        //deleteButton.setText(roleName);
+                        //docData.put("sun","closed");
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        Map<String, Object> docData = new HashMap<>();
+                        docData.put("name",nameService);
+                        docData.put("role",roleName);
+
+                        database.collection("users").document(user.getEmail()).collection("services").document(nameService).set(docData);
+
+
+
+                    }
+                });
+
+
+
+                //});
+            }
+        });
         return view;
+
     }
 }
