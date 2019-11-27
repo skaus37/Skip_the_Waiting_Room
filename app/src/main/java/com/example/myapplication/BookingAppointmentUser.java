faa5e9286bde027303651dee466d8ead6bb2457b
Angus Young <angusryoung@gmail.com>
2019-11-27 17:26:41 -0500

    added calander veiw

--- app/src/main/java/com/example/myapplication/BookingAppointmentUser.java
+++ app/src/main/java/com/example/myapplication/BookingAppointmentUser.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
 
 public class BookingAppointmentUser extends AppCompatActivity {
     //represents start and end time of a particular day
@@ -16,6 +19,9 @@ public class BookingAppointmentUser extends AppCompatActivity {
     int startMin;
     int endHour;
     int endMin;
+
+    //current day
+    String currentDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +40,52 @@ public class BookingAppointmentUser extends AppCompatActivity {
         });
     }
 
-    //takes information from the selected day of the week and intializes startHour, endHour etc....
+    //takes information from the selected day of the week and initialize startHour, endHour etc....
     public void parseTimes(){
+
+    }
+
+    public void getDayofWeek(){
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year, month, dayOfMonth);
+                int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+                switch (dayOfWeek ) {
+                    case Calendar.SUNDAY:
+                        currentDay= "sun";
+                    case Calendar.MONDAY:
+                        currentDay= "mon";
+                    case Calendar.TUESDAY:
+                        currentDay= "sun";
+                    case Calendar.WEDNESDAY:
+                        currentDay= "tues";
+                    case Calendar.THURSDAY:
+                        currentDay= "thurs";
+                    case Calendar.FRIDAY:
+                        currentDay= "fri";
+                    case Calendar.SATURDAY:
+                        currentDay= "sat";
+
+
+
+                }
+
+
+
+
+            }
+        });
+
+
+
+
+
 
     }
 
